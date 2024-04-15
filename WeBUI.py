ff7a9e9327644fabdac274e54cf991f7b9b599d9
Calvin_Kong <kentkong0@gmail.com>
2024-04-15 14:59:00 +0800

    WebUI 1.0.3: number of pages shown

--- WeBUI.py
+++ WeBUI.py
@@ -12,12 +12,14 @@ def home():
 @app.route('/search', methods=['POST'])
 def search():
     query = request.form['query']
-    with open("spider_result.txt", 'r') as file:
+    with open("spider_result.txt", 'r', encoding="utf-8") as file:
         content = file.read()
+    with open(r"spider_result.txt", "r", encoding="utf-8") as file:
+        count = int(len(file.readlines())/8)
     # Perform search operations here
     content_with_links = re.sub(r'(https?://\S+)', r'<a href="\1">\1</a>', content)
     # Render search results
-    return render_template('results.html', content=content_with_links, query=query)
+    return render_template('results.html', content=content_with_links, query=query, numOfPage=count)
 
 if __name__ == '__main__':
     app.run(debug=True)
