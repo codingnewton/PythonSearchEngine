532b2e0301f887bd1e168912842724e6150d3016
Calvin_Kong <kentkong0@gmail.com>
2024-04-21 23:11:39 +0800

    main function integrated to WeBUI, ranking needed

--- WeBUI.py
+++ WeBUI.py
@@ -1,17 +1,26 @@
 from flask import Flask, render_template, request
 import re
+from scraper import page, HTML_list
 
 app = Flask(__name__)
-
+spider = HTML_list()
 # Home page
 @app.route('/')
 def home():
+    spider.crawl("https://www.cse.ust.hk/~kwtleung/COMP4321/testpage.htm", 300)
+    filename = "indexertest.db"
+    spider.createdb(filename)
+    spider.dbforward(filename)
+    spider.dbinverted(filename)
+
     return render_template('index.html')
 
 # Search page
 @app.route('/search', methods=['POST'])
 def search():
     query = request.form['query']
+    #sorting/ranking operation
+    spider.export('return')
     with open("spider_result.txt", 'r', encoding="utf-8") as file:
         content = file.read()
     with open(r"spider_result.txt", "r", encoding="utf-8") as file:
